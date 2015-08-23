bd86e0722a867fb88be05854f4cd8ff799ff7e4d
Reccy <aaronmeaney@hotmail.com>
2015-08-23 15:54:01 +0100

    Database Client GUI done.

--- Assets/Scripts/DatabaseClientScript.cs
+++ Assets/Scripts/DatabaseClientScript.cs
@@ -11,14 +11,26 @@ public class DatabaseClientScript : MonoBehaviour {
     private Text firstnameText;
     private Text surnameText;
     private Text subjectText;
+    private Text cardnameText;
+    private Text cardnumText;
+    private Text expirationdateText;
+    private Text codeText;
+    private Text statusText;
+    private InputField cardnameInput;
+    private InputField cardnumInput;
+    private InputField expirationdateInput;
+    private InputField codeInput;
+    private Button submitBtn;
+    private Button clientBtn;
     private string ID;
     private string firstname;
     private string surname;
     private string subject;
-    private string cardname;
-    private string cardnum;
-    private string expirationdate;
-    private string code;
+    public string cardname;
+    public string cardnum;
+    public string expirationdate;
+    public string code;
+    private bool cardAccepted;
 
 	void Start () 
     {
@@ -29,6 +41,18 @@ public class DatabaseClientScript : MonoBehaviour {
         firstnameText = GameObject.FindGameObjectWithTag("Firstname").GetComponent<Text>();
         surnameText = GameObject.FindGameObjectWithTag("Surname").GetComponent<Text>();
         subjectText = GameObject.FindGameObjectWithTag("Subject").GetComponent<Text>();
+        cardnameText = GameObject.FindGameObjectWithTag("Cardname").GetComponent<Text>();
+        cardnumText = GameObject.FindGameObjectWithTag("Cardnum").GetComponent<Text>();
+        expirationdateText = GameObject.FindGameObjectWithTag("ExpirationDate").GetComponent<Text>();
+        codeText = GameObject.FindGameObjectWithTag("Code").GetComponent<Text>();
+        statusText = GameObject.FindGameObjectWithTag("Status").GetComponent<Text>();
+        cardnameInput = GameObject.FindGameObjectWithTag("CardNameInput").GetComponent<InputField>();
+        cardnumInput = GameObject.FindGameObjectWithTag("CardNumberInput").GetComponent<InputField>();
+        expirationdateInput = GameObject.FindGameObjectWithTag("ExpirationDateInput").GetComponent<InputField>();
+        codeInput = GameObject.FindGameObjectWithTag("SecurityCodeInput").GetComponent<InputField>();
+        submitBtn = GameObject.FindGameObjectWithTag("SubmitBtn").GetComponent<Button>();
+        clientBtn = GameObject.FindGameObjectWithTag("ClientBtn").GetComponent<Button>();
+        cardAccepted = false;
 	}
 
     public void ExitButtonClicked()
@@ -51,5 +75,83 @@ public class DatabaseClientScript : MonoBehaviour {
         firstnameText.text = "Firstname: " + firstname;
         surnameText.text = "Surname: " + surname;
         subjectText.text = "Subject :" + subject;
+    }
+
+    public void nextDetails()
+    {
+        peopleInformation.nextPerson();
+        statusText.text = "";
+        cardAccepted = false;
+        cardnameInput.interactable = true;
+        cardnameInput.text = "";
+        cardnumInput.interactable = true;
+        cardnumInput.text = "";
+        expirationdateInput.interactable = true;
+        expirationdateInput.text = "";
+        codeInput.interactable = true;
+        codeInput.text = "";
+        submitBtn.interactable = true;
+        clientBtn.interactable = false;
+    }
+
+    public void verifyDetails()
+    {
+        int correctNum = 0;
+
+        if(cardnameText.text != cardname)
+        {
+            Debug.Log("WRONG NAME");
+        }
+        else
+        {
+            Debug.Log("RIGHT NAME");
+            correctNum++;
+        }
+
+        if (cardnumText.text != cardnum)
+        {
+            Debug.Log("WRONG NUM");
+        }
+        else
+        {
+            Debug.Log("RIGHT NUM");
+            correctNum++;
+        }
+
+        if (expirationdateText.text != expirationdate)
+        {
+            Debug.Log("WRONG DATE");
+        }
+        else
+        {
+            Debug.Log("RIGHT DATE");
+            correctNum++;
+        }
+
+        if (codeText.text != code)
+        {
+            Debug.Log("WRONG CODE");
+        }
+        else
+        {
+            Debug.Log("RIGHT CODE");
+            correctNum++;
+        }
+
+        if(correctNum == 4)
+        {
+            statusText.text = "<color=#33BB33>CARD INFORMATION ACCEPTED!</color>";
+            cardAccepted = true;
+            cardnameInput.interactable = false;
+            cardnumInput.interactable = false;
+            expirationdateInput.interactable = false;
+            codeInput.interactable = false;
+            submitBtn.interactable = false;
+            clientBtn.interactable = true;
+        }
+        else
+        {
+            statusText.text = "<color=#BB3333>CARD INFORMATION DENIED!</color>";
+        }
     }
 }
--- Assets/Scripts/PeopleInformation.cs
+++ Assets/Scripts/PeopleInformation.cs
@@ -15,23 +15,23 @@ public class PeopleInformation : MonoBehaviour {
 
     public void nextPerson()
     {
-        if(personCurrent == 0)
+        personCurrent++;
+
+        if(personCurrent == 1)
         {
             databaseClient.setDetails(Random.Range(1000, 5000).ToString(), "James", "Johnson", "Ransomware", "James Johnson", Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString(), Random.Range(1, 12).ToString() + "/" + Random.Range(97, 99).ToString(), Random.Range(100, 900).ToString());
         }
-        else if(personCurrent == 1)
+        else if(personCurrent == 2)
         {
             databaseClient.setDetails(Random.Range(1000, 5000).ToString(), "Fuck", "You", "I'm not a pedo", "Richard Jenkins", Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString(), Random.Range(1, 12).ToString() + "/" + Random.Range(97, 99).ToString(), Random.Range(100, 900).ToString());
         }
         else if(personCurrent == 3)
         {
-            databaseClient.setDetails(Random.Range(1000, 5000).ToString(), "Moira", "O'Flannigan", "USafe AntiVirus", "Dick O'Flannigan", Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString(), Random.Range(1, 12).ToString() + "/" + Random.Range(97, 99).ToString(), Random.Range(100, 900).ToString());
+            databaseClient.setDetails(Random.Range(1000, 5000).ToString(), "Moira", "O'Flannigan", "USafe AntiVirus", "Brad O'Flannigan", Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString() + "-" + Random.Range(1000, 5000).ToString(), Random.Range(1, 12).ToString() + "/" + Random.Range(97, 99).ToString(), Random.Range(100, 900).ToString());
         }
         else
         {
             //End Game
         }
-
-        personCurrent++;
     }
 }
