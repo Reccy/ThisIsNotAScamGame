c4752e43dbab0f429160a223285004f6d2d880ce
Reccy <aaronmeaney@hotmail.com>
2016-07-13 18:40:18 +0100

    Added support for multiple resolutions. Locked to 4:3 aspect ratio.

--- Assets/Scripts/SetUIScale.cs
+++ Assets/Scripts/SetUIScale.cs
@@ -4,18 +4,12 @@ using System.Collections;
 
 public class SetUIScale : MonoBehaviour {
 
-    int screenWidth = Screen.width; //Screen width
+    int screenWidth = Screen.width;
     public Canvas canvas;
     private CanvasScaler canvasScaler;
 
 	void Start () {
         canvasScaler = canvas.GetComponent<CanvasScaler>();
-        if(screenWidth < 1024)
-        {
-            canvasScaler.scaleFactor = 1/(1024f/screenWidth);
-        }
-        Debug.Log("Screen Width: " + screenWidth);
-        Debug.Log("Target Size: " + 1/(1024f/screenWidth));
-        Debug.Log("Canvas Scalefactor: " + canvasScaler.scaleFactor);
+        canvasScaler.scaleFactor = 1/(1024f/screenWidth);
 	}
 }
