8fde172bb874329b318c2ab27dbcb5e44a8e4ef0
Reccy <aaronmeaney@hotmail.com>
2016-07-13 18:03:43 +0100

    Added UI Scaling

---
+++ Assets/Scripts/SetUIScale.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetUIScale : MonoBehaviour {
+
+    int screenWidth = Screen.width; //Screen width
+    public Canvas canvas;
+    private CanvasScaler canvasScaler;
+
+	void Start () {
+        canvasScaler = canvas.GetComponent<CanvasScaler>();
+        if(screenWidth < 1024)
+        {
+            canvasScaler.scaleFactor = 1/(1024f/screenWidth);
+        }
+        Debug.Log("Screen Width: " + screenWidth);
+        Debug.Log("Target Size: " + 1/(1024f/screenWidth));
+        Debug.Log("Canvas Scalefactor: " + canvasScaler.scaleFactor);
+	}
+}
