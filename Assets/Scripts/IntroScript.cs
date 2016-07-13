06e747c94ca4bfdce0a6bce4e9bf15cef0e1a08a
Reccy <aaronmeaney@hotmail.com>
2016-07-13 21:52:31 +0100

    Layed out login screen

--- Assets/Scripts/IntroScript.cs
+++ Assets/Scripts/IntroScript.cs
@@ -1,86 +1,61 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
 public class IntroScript : MonoBehaviour {
 
-    private List<string> l;
+    private List<string> preList; //List for initial rll-through of commands
+    private List<string> introList; //List for initial game screen
     private Text term;
 
 	void Start () 
     {
-        Debug.Log("SCRIPT STARTED");
         term = GameObject.Find("Terminal").GetComponent<Text>();
 
-	    l = new List<string>();
-        l.Add("\nGetting OS \"Wandisk 97\"");
-        l.Add("\nInitating OS");
-        l.Add("\nBooting OS");
-        l.Add("\nRunning Custom Script \"adminbackdoor.exe\"");
-        l.Add("\nUser RILEY@MF logged in");
-        l.Add("\nERROR: No audio driver detected!");
-        l.Add("\nRunning \"mf_db.exe\"");
-        l.Add("\nRunning \"chat_admin.exe\"");
-        l.Add("\nCustom Script Complete");
-        l.Add("\n");
-        l.Add("\nINSTRUCTION FROM ADMIN");
-        l.Add("\n======================");
-        l.Add("\nGet the client's card info");
-        l.Add("\nInsert it into our database");
-        l.Add("\nIf you lose a client, skip the entry");
-        l.Add("\nDon't give in to sympathy");
-        l.Add("\nYou are the Monster");
-        l.Add("\nPRESS <ENTER> TO BEGIN");
+        //Add data to pre list
+	    preList = new List<string>();
+        preList.Add("POWER GOOD");
+        preList.Add("CHECKING FOR VALID OPERATING SYSTEM");
+        preList.Add("FOUND WANDISK 97");
+        preList.Add("LOADING WANDISK 97");
+        preList.Add("FETCHING RESOURCE MANIFEST");
+        preList.Add("LOADING OPERATING SYSTEM RESOURCES");
+        preList.Add("Hpe5krXx1fsdfs3f3wfvYIkBbX38NB");
+        preList.Add("irPQE4pIw3fwfw55ywabCbb7wjTMu");
+        preList.Add("W45lj1yzDdbsdfsdAMDVDdVg1");
+        preList.Add("FnH3EttXwrjgdgsvz7pSAzfyiC8");
+        preList.Add("8hHjIzYMvavasvvc0CE6t6AhxF");
+        preList.Add("pvPhkorEXjrtjPkmvz5NsvvT");
+        preList.Add("Lk6mdMBwxk9YIdHlwBgB");
+        preList.Add("tKfB2WMEEib5262342gQPM1Q4wMpp");
+        preList.Add("vA2uOjdgLK4hsbK4m3mOQdB8");
+        preList.Add("gJfhD7WS54s2eqVQ8NvWvp");
+        preList.Add("oF1QMNWpwegaHzY9GtPQLt");
+        preList.Add("xtZ4wPEhTe3tTTTsbx1vZ2lmx0z");
+        preList.Add("vrwTu8tRkJcbxcvcsvhbI7Z0u");
+        preList.Add("QP0Zu7KZDgdfsdfw0dTrwMIs9");
+        preList.Add("SaL0Xuh23WmsdfsIzL3s1EQS");
+        preList.Add("9YbLboPGUHsdfNcUzvE2nVp");
+        preList.Add("KjOKj73dRwesdfsfsZ8VgLSrsS");
+        preList.Add("gsnsHSi4zSv422342424tgsf634pVTgrBKT2L");
+        preList.Add("d6yJDDBk423inQ4d9RXP5JH");
+        preList.Add("ubVOeRwLSAMmQ5opOUB0");
+        preList.Add("RESOURCES LOADED");
+        preList.Add("STARTING WANDISK 97...");
+        //Start intro
         StartCoroutine(bootUp());
 	}
 
-    void Update() 
-    { 
-        if(Input.GetKeyDown("enter") || Input.GetKeyDown("return"))
-        {
-            Application.LoadLevel("MainScene");
-        }
-    }
-
     IEnumerator bootUp()
     {
-        Debug.Log("BOOTING");
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[0];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[1];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[2];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[3];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[4];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[5];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[6];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[7];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[8];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[9];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[10];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[11];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[12];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[13];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[14];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[15];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[16];
-        yield return new WaitForSeconds(0.5f);
-        term.text = term.text + l[17];
+        term.text = preList[0];
+        for(int i = 1; i < preList.Count; i++)
+        {
+            term.text = term.text + "\n" + preList[i];
+            yield return new WaitForSeconds(0.1f);
+        }
+        SceneManager.LoadScene("IntroScene");
     }
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
